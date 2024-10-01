f13908aa34cd805e9fd33bd528fdf437c4c7759c
Norvez <normanalvarado123@gmail.com>
2024-09-30 20:53:59 -0600

    Refactor App.tsx to add AuthProvider and wrap AppRouter component

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import 'leaflet/dist/leaflet.css';
 import 'flowbite/dist/flowbite.css';
 import AppRouter from './routers/AppRoter';
 import { ThemeProvider } from './components/ThemeContext';
-
-
+import { AuthProvider } from './contexts/AuthContext';
 
 function App() {
   return (
     <ThemeProvider>
-      <AppRouter />
+      <AuthProvider> 
+        <AppRouter />
+      </AuthProvider>
     </ThemeProvider>
   );
 }
